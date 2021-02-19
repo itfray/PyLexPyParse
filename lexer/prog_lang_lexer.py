ea1fe03aa6a63919baa878d2a4be3730c6e7e210
Efarov Ilya <efarov.ily@mail.ru>
2021-02-19 23:25:29 +0900

    doc prog_lang_lexer

--- lexer/prog_lang_lexer.py
+++ lexer/prog_lang_lexer.py
@@ -2,10 +2,13 @@ from .lexer import Lexer
 
 
 class ProgLangLexer(Lexer):
-    skip_kind: str
-    id_kind: str
-    keyword_kind: str
-    __keywords: tuple
+    """
+    ProgLangLexer is lexical analyzer of programming language for strings analyzing
+    """
+    skip_kind: str                                              # kind of lexeme that need skip
+    id_kind: str                                                # kind of lexeme that is identifier
+    keyword_kind: str                                           # kind of lexeme taht is keyword
+    __keywords: tuple                                           # keywords of programming language
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,18 +20,27 @@ class ProgLangLexer(Lexer):
     def tokens(self):
         for token in super().tokens():
             if token.kind == self.skip_kind:
-                continue
+                continue                                        # skip token
             elif token.kind == self.id_kind:
-                if token.value in self.keywords:
+                if token.value in self.keywords:                # check identifier or keyword
                     token.kind = self.keyword_kind
-            yield token
+            yield token                                         # return token
 
     @property
     def keywords(self)-> tuple:
+        """
+        Get keywords of programming language
+        :return: tuple of keywords
+        """
         return self.__keywords
 
     @keywords.setter
     def keywords(self, value: tuple)-> None:
+        """
+        Set keywords of programming language
+        :param value: tuple of keywords
+        :return: None
+        """
         if value is None:
             raise ValueError('keywords can not be None!!!')
         self.__keywords = value
