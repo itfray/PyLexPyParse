90177e770ad46c5434753fdc5c42a123dfd7b944
Efarov Ilya <efarov.ily@mail.ru>
2021-02-20 12:54:08 +0900

    fetch multicomments in pascal abc

--- lexer/prog_lang_lexer.py
+++ lexer/prog_lang_lexer.py
@@ -1,15 +1,23 @@
 from .lexer import Lexer
+import re
+from collections import namedtuple
+
+
+MultiTokenBounds = namedtuple('MultiTokenBounds', "start end")
+MultiTokenBound = namedtuple('MultiTokenBound', "value regex")
 
 
 class ProgLangLexer(Lexer):
     """
     ProgLangLexer is lexical analyzer of programming language for strings analyzing
     """
-    skip_kind: str                                              # kind of lexeme that need skip
-    id_kind: str                                                # kind of lexeme that is identifier
-    keyword_kind: str                                           # kind of lexeme taht is keyword
-    __keywords: tuple                                           # keywords of programming language
+    skip_kind: str                              # kind of lexeme that need skip
+    id_kind: str                                # kind of lexeme that is identifier
+    keyword_kind: str                           # kind of lexeme taht is keyword
+    __keywords: tuple                           # keywords of programming language
     case_sensitive: bool
+    __multitokens: dict                         # {'KIND': (start=(value='...', regex='...'), end=(...)), ...}
+    __multiregexs: dict
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,17 +26,40 @@ class ProgLangLexer(Lexer):
         self.keyword_kind = kwargs.get("keyword_kind", "")
         self.keywords = kwargs.get("keywords", ())
         self.case_sensitive = kwargs.get("case_sensitive", True)
+        self.multitokens = kwargs.get("multitokens", dict())            # re.compile(r'(?P<>.+)')
 
     def tokens(self):
-        for token in super().tokens():
-            if token.kind == self.skip_kind:
-                continue                                            # skip token
-            elif token.kind == self.id_kind:
-                if not self.case_sensitive:
-                    token.value = token.value.lower()
-                if token.value in self.keywords:                             # check identifier or keyword
-                    token.kind = self.keyword_kind
-            yield token                                             # return token
+        total_regex = self._Lexer__token_regex
+        token_gen = super().tokens()
+        try:
+            while True:
+                token = next(token_gen)
+                if token.kind == self.skip_kind:
+                    token = None                                            # skip token
+                elif token.kind == self.id_kind:
+                    if not self.case_sensitive:
+                        token.value = token.value.lower()
+                    if token.value in self.__keywords:                       # check identifier or keyword
+                        token.kind = self.keyword_kind
+                elif token.kind in self.__multitokens:
+                    kind = token.kind
+                    bounds = self.__multitokens[kind]
+                    if token.value != bounds.start.value and \
+                       bounds.start.value != bounds.end.value:
+                        raise UnknownLexemeError(f"Unexcepted character '{token.value}'" +
+                                                 f" in line {self.num_line} in column {self.num_column}!!!")
+                    yield token
+                    self._Lexer__token_regex = self.__multiregexs[kind]
+                    while token.value != bounds.end.value:
+                        token = next(token_gen)
+                        token.kind = kind
+                        yield token
+                    self._Lexer__token_regex = total_regex
+                    token = None
+                if not token is None:
+                    yield token
+        except StopIteration:
+            self._Lexer__token_regex = total_regex
 
     @property
     def keywords(self)-> tuple:
@@ -48,3 +79,17 @@ class ProgLangLexer(Lexer):
         if value is None:
             raise ValueError('keywords can not be None!!!')
         self.__keywords = value
+
+    @property
+    def multitokens(self)-> dict:
+        return self.__multitokens.copy()
+
+    @multitokens.setter
+    def multitokens(self, value: dict)-> None:
+        if value is None:
+            raise ValueError('multitokens can not be None!!!')
+        self.__multitokens = value
+        self.__multiregexs = dict()
+        for kind in self.__multitokens:
+            regex = self.__multitokens[kind].end.regex
+            self.__multiregexs[kind] = re.compile('((%s)|([^%s]+))' % (regex, regex))
--- pascalabc_lexer.py
+++ pascalabc_lexer.py
@@ -1,6 +1,6 @@
 from str_reader.str_reader import StrReader
 from str_reader.file_reader import FileStrReader
-from lexer.prog_lang_lexer import ProgLangLexer
+from lexer.prog_lang_lexer import ProgLangLexer, MultiTokenBounds, MultiTokenBound
 from time import time
 
 
@@ -24,12 +24,23 @@ KEYWORDS = ('and', 'array', 'as', 'begin',
             'xor', 'abstract', 'default', 'external', 'internal', 'on',
             'overload', 'override', 'params', 'read', 'reintroduce',
             'virtual', 'write')
+
+MULTICOMMENT_KIND1 = "MULTI_COMENT1"
+MULTICOMMENT_BOUNDS1 = MultiTokenBounds(MultiTokenBound('{', r'{'),
+                                        MultiTokenBound('}', r'}'))
+MULTICOMMENT_KIND2 = "MULTI_COMENT2"
+MULTICOMMENT_BOUNDS2 = MultiTokenBounds(MultiTokenBound('(*', r'\(\*'),
+                                        MultiTokenBound('*)', r'\*\)'))
+MULTITOKENS = {
+    MULTICOMMENT_KIND1: MULTICOMMENT_BOUNDS1,
+    MULTICOMMENT_KIND2: MULTICOMMENT_BOUNDS2,
+}
+
 SPECIFICATION = [
     (SKIP_KIND, r'[\s\t]+'),
-    # ('COMMENT', r'({.*}|\(\*.*\*\)|//.*?)'),
-    # ('COMMENT', r'(//.*)'),
-    # ('COMMENT', r'{.*'),
-    ('COMMENT', r'(//.*)'),
+    (MULTICOMMENT_KIND1, MULTICOMMENT_BOUNDS1.start.regex),
+    (MULTICOMMENT_KIND2, MULTICOMMENT_BOUNDS2.start.regex),
+    ('LINE_COMMENT', r'//.*'),
     (ID_KIND, r'(_|\w){1}(_|\w|\d)*'),
     ('NUM', r'(\+|-)?\d+(\.\d+)?((e|E)(\+|-)?\d+)?'),
     ('HEX_NUM', r'\$[A-Fa-f0-9]+'),
@@ -62,27 +73,35 @@ if __name__ == "__main__":
     begin
         a := 0;
         if (a) then
-            _b := -123.0;       // Hello, my world!!!
+            _b := -123.0;           // Установить значение для _b!!!
         result := +3.3E+6;
-        kek := $C000fd;
-        result := kek / result;
+        kek := $C000fd;             (* Установить  значение для kek!!! *)
+        result := kek / result;     { Вычислить result!!! }
     end.
-    """
 
-    """
     {
     
         Многострочный комментарий!!!
     
     }
+    
+    (*
+    
+        Это тоже многострочный коменнтарий!!!
+        
+    *)
+    
     """
     data_reader = StrReader(stmt)
 
     lexer = ProgLangLexer(data_reader=data_reader, specification=SPECIFICATION,
                           skip_kind=SKIP_KIND, keyword_kind=KEYWORD_KIND,
                           id_kind=ID_KIND, keywords=KEYWORDS,
+                          multitokens=MULTITOKENS,
                           case_sensitive=CASE_SENSITIVE)
     t0 = time()
+    tokens = []
     for token in lexer.tokens():
         print(f"{lexer.num_line}:{lexer.num_column}: {token}")
+        tokens += [token]
     print(time() - t0, " sec")
