b22f9fccc735809d7f245fcdf4a456db5a09ecf0
Efarov Ilya <efarov.ily@mail.ru>
2021-02-22 15:45:10 +0900

    fix IParser

--- parser/iparser.py
+++ parser/iparser.py
@@ -3,9 +3,9 @@ from lexer import ILexer
 
 
 class Node:
-    value = None
-    parent: Node
-    __childs: list
+    value = None                        # node value
+    parent: Node                        # parent node
+    __childs: list                      # child nodes
     def __init__(self, **kwargs):
         self.parent = kwargs.get('parent', None)
         self.value = kwargs.get('value', None)
@@ -14,10 +14,18 @@ class Node:
 
     @property
     def childs(self)-> list:
+        """
+        Get list child nodes
+        :return:
+        """
         return self.__childs
 
 
 class IParser(abc.ABC):
+    """
+    IParser is interface of syntax analyzer
+    """
+    @abc.abstractmethod
     def parse(self)-> Node:
         """
         Parses tokens and constructs parse tree
