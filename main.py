7645a9cdb3ae95294bd877fbcec1bc62c4a40f28
Efarov Ilya <efarov.ily@mail.ru>
2021-02-25 23:28:13 +0900

    fix

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-from sparser.sparser import SParser, CellSParseTab, SParseTab
+from str_reader.str_reader import StrReader
+from lexer.prog_lang_lexer import ProgLangLexer
+from sparser.sparser import SParser
+from time import time
 
 
-GOAL_NTERM = "S'"
+CASE_SENSITIVE = False
+SKIP_KIND = 'SKIP'
+ID_KIND = 'ID'
+
+SPECIFICATION = [
+    (SKIP_KIND, r'[\s\t]+'),
+    (ID_KIND, r'[_A-Za-zА-Яа-я&][_A-Za-zА-Яа-я\d]*'),
+    ('OP_ARTHM', r'(\*\*|[-\*\+/])'),
+    ('DELIM', r'[:;,()\.\[\]]'),
+]
+
+stmt = """ A * B + C"""
+data_reader = StrReader(stmt)
+lexer = ProgLangLexer(data_reader=data_reader,
+                      specification=SPECIFICATION,
+                      skip_kind=SKIP_KIND,
+                      id_kind=ID_KIND,
+                      case_sensitive=CASE_SENSITIVE)
+# t0 = time()
+# try:
+#     for token in lexer.tokens():
+#         print(f"{lexer.num_line}:{lexer.num_column}: {token}")
+# except UnexceptedLexemeError as err:
+#     print(err)
+# print(time() - t0, " sec")
+
+GOAL_NTERM = "E'"
 END_TERM = '⊥'
 RULES = """
-         S' -> S;
-         S -> C C;
-         C -> c C |
-              d
+         E' -> E;
+         E -> E '+' T |
+              E '-' T |
+              T;
+         T -> T '*' F |
+              T '/' F |
+              F;
+         F -> '('E')' |
+              ID
         """
-TOKENS = ('c', 'd')
+TOKENS = ('ID',)
 
-parser = SParser(tokens=TOKENS, goal_nterm=GOAL_NTERM, end_term=END_TERM)
+parser = SParser(lexer=lexer,
+                 tokens=TOKENS,
+                 goal_nterm=GOAL_NTERM,
+                 end_term=END_TERM)
 parser.parse_rules_from(RULES)
 parser.create_sparse_tab()
-print(parser.rules)
+node = parser.parse()
+print(node)
--- sparser/sparser.py
+++ sparser/sparser.py
@@ -909,3 +909,20 @@ class SParser(ISParser):
                 rules.append(rule)
                 index += 1
         return rules
+
+
+if __name__ == "__main__":
+    GOAL_NTERM = "S'"
+    END_TERM = '⊥'
+    RULES = """
+             S' -> S;
+             S -> C C;
+             C -> c C |
+                  d
+            """
+    TOKENS = ('c', 'd')
+
+    parser = SParser(tokens=TOKENS, goal_nterm=GOAL_NTERM, end_term=END_TERM)
+    parser.parse_rules_from(RULES)
+    parser.create_sparse_tab()
+    print(parser.rules)
