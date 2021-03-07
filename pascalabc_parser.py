8d4839d3df204768c56fc7d2bfd049b0ebe92a83
Efarov Ilya <efarov.ily@mail.ru>
2021-03-07 10:41:35 +0900

    need add section_var pascal parser

--- pascalabc_parser.py
+++ pascalabc_parser.py
@@ -3,17 +3,75 @@ GOAL_NTERM = "program"
 END_TERM = '⊥'
 EMPTY_TERM = 'ε'
 RULES = """
-        program -> section_prgrname section_uses section_program;
+        program -> section_prgrname section_uses sub_sections section_program;
+
+
+        sub_sections -> sub_sections sub_section| 
+                        ε;
         
+
+        sub_section -> section_var |
+                       section_const |
+                       section_type |
+                       section_label;
+        
+
         section_prgrname -> 'program' ID ';' |
+                            'unit' ID ';' |
                              ε;
-                             
+                
+
         section_uses -> section_uses uses |
                         ε;
-        uses -> 'uses' modules ';';
-        modules -> modules ',' module |
-                   module;
-        module -> ID;
+        uses -> 'uses' list_ids ';';
+        list_ids -> list_ids ',' ID |
+                    ID;
+
+
+        section_var -> 'var' decls;
+        decls -> decls decl |
+                 decl;
+        list_exprs -> list_exprs ',' expr |
+                      expr;
+        decl -> list_ids ':' ID ';' |
+                ID ':' ID ':=' expr ';' |
+                ID ':' ID '=' expr ';' |
+                ID ':=' expr ';' |
+                '(' list_ids ')' ':=' '(' list_exprs ')' ';';
+
+
+        section_const -> 'const';
+
+
+        section_type -> 'type';
+
+
+        section_label -> 'label';
+        
+
+        section_program -> block '.';
+        block -> 'begin' stmts 'end';
+        stmts -> stmts stmt |
+                 ε;
+        stmt -> ';';
+        expr -> ID |
+                NUM
+        """
+RULES = """
+        program -> section_prgrname section_uses section_program;
+        
+
+        section_prgrname -> 'program' ID ';' |
+                            'unit' ID ';' |
+                             ε;
+                
+
+        section_uses -> section_uses uses |
+                        ε;
+        uses -> 'uses' list_ids ';';
+        list_ids -> list_ids ',' ID |
+                    ID;
+        
         
         section_program -> block '.';
         block -> 'begin' stmts 'end';
@@ -21,7 +79,7 @@ RULES = """
                  ε;
         stmt -> ';'
         """
-TOKENS = ('ID',)
+TOKENS = ('ID', 'NUM',)
 STAB_FILENAME = "pascalabc_stab.prstab"
 
 
@@ -29,7 +87,7 @@ if __name__ == "__main__":
     from time import time
     from str_reader.file_reader import FileStrReader
     from lexer.prog_lang_lexer import ProgLangLexer, UnexceptedLexError
-    from sparser.sparser import SParser, print_sparse_tab
+    from sparser.sparser import SParser
     from pascalabc_lexer import (CASE_SENSITIVE, SKIP_KIND,
                                  SPECIFICATION, ID_KIND,
                                  KEYWORD_KIND, KEYWORDS,
@@ -67,7 +125,6 @@ if __name__ == "__main__":
     print(parser.rules)
     print()
     parser.create_sparse_tab()
-    # print_sparse_tab(parser, 17)
     t0 = time()
     node = parser.parse()
     print("parsing time: ", time() - t0, " sec")
